29820517b19dba8d93eac98a6c4de26833eb9e82
mahaabi01 <mahaabi01@gmail.com>
2026-06-02 20:47:16 +0545

    removed cached unnecessary file

---
+++ DlmsWebApi/DlmsWebApi.Business/CategoryBusiness/ICategoryBusiness.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DlmsWebApi.Business.CategoryBusiness
+{
+    internal interface ICategoryBusiness
+    {
+    }
+}
---
+++ DlmsWebApi/DlmsWebApi.Shared/CategoryData/CategoryDetails.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DlmsWebApi.Shared.CategoryData
+{
+    internal class CategoryDetails
+    {
+    }
+}
