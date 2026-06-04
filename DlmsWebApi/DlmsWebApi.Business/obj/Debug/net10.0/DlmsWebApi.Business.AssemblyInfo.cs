eabcca4273806e4d58cfe3fa121e098c1a6c6b99
Rajesh Parajuli <rajeshparajuli146@gmail.com>
2026-06-04 14:04:46 +0545

    remove obj cache files

--- DlmsWebApi/DlmsWebApi.Business/obj/Debug/net10.0/DlmsWebApi.Business.AssemblyInfo.cs
+++ DlmsWebApi/DlmsWebApi.Business/obj/Debug/net10.0/DlmsWebApi.Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DlmsWebApi.Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1c939e8cb869613f668210a20da716bccf7b8e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4ee9c0cc41d661a0580270d0d69ae87cffe035e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DlmsWebApi.Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DlmsWebApi.Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
